0b3c0465596330fb16dafda3996d595bfaf18c23
Vikhun15 <okv.pocokfej@gmail.com>
2023-01-16 13:19:58 +0100

    Termek elhelyezése

--- scripts.js
+++ scripts.js
@@ -7,6 +7,7 @@ $("#mapframe").on("load" ,function(){
     })
 })
 
+
 function resizeIframe(that){
     that.style.height=(that.contentWindow.document.body.scrollHeight+20)+'px';
 }
@@ -33,6 +34,7 @@ function galChange(that){
 }
 
 function iframeLoaded(){
+    var isktxt = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent laoreet tincidunt tortor, quis dignissim lacus imperdiet eu. Praesent mollis aliquet justo non elementum. Morbi dignissim orci eget ullamcorper pretium. Etiam in massa laoreet, iaculis leo sit amet, blandit leo. Maecenas vitae leo in elit ullamcorper pharetra ac eget ligula."
     $("#le", parent.document).bind("click", function(){
         if($("#szint", parent.document).attr("value") === "sat"){
             $("#map2").hide()
@@ -41,6 +43,30 @@ function iframeLoaded(){
             $("#szint", parent.document).text("Épületek")
 
         }
+        else if($("#szint", parent.document).attr("value") === "div"){
+            $("#epuletek").hide()
+            $("#em3").css("display", "")
+            $("#szint", parent.document).attr("value", "em3")
+            $("#szint", parent.document).text("3. emelet")
+        }
+        else if($("#szint", parent.document).attr("value") === "em3"){
+            $("#em3").hide()
+            $("#em2").css("display", "")
+            $("#szint", parent.document).attr("value", "em2")
+            $("#szint", parent.document).text("2. emelet")
+        }
+        else if($("#szint", parent.document).attr("value") === "em2"){
+            $("#em2").hide()
+            $("#em1").css("display", "")
+            $("#szint", parent.document).attr("value", "em1")
+            $("#szint", parent.document).text("1. emelet")
+        }
+        else if($("#szint", parent.document).attr("value") === "em1"){
+            $("#em1").hide()
+            $("#fsz").css("display", "")
+            $("#szint", parent.document).attr("value", "fsz")
+            $("#szint", parent.document).text("Földszint")
+        }
     })
     $("#fel", parent.document).bind("click", function(){
         if($("#szint", parent.document).attr("value") === "div"){
@@ -48,6 +74,30 @@ function iframeLoaded(){
             $("#epuletek").hide()
             $("#szint", parent.document).attr("value", "sat")
             $("#szint", parent.document).text("Szatellit")
+        }
+        else if($("#szint", parent.document).attr("value") === "em3"){
+            $("#epuletek").css("display", "")
+            $("#em3").hide()
+            $("#szint", parent.document).attr("value", "div")
+            $("#szint", parent.document).text("Épületek")
+        }
+        else if($("#szint", parent.document).attr("value") === "em2"){
+            $("#em3").css("display", "")
+            $("#em2").hide()
+            $("#szint", parent.document).attr("value", "em3")
+            $("#szint", parent.document).text("3. emelet")
+        }
+        else if($("#szint", parent.document).attr("value") === "em1"){
+            $("#em2").css("display", "")
+            $("#em1").hide()
+            $("#szint", parent.document).attr("value", "em2")
+            $("#szint", parent.document).text("2. emelet")
+        }
+        else if($("#szint", parent.document).attr("value") === "fsz"){
+            $("#em1").css("display", "")
+            $("#fsz").hide()
+            $("#szint", parent.document).attr("value", "em1")
+            $("#szint", parent.document).text("1. emelet")
         }
     })
 
@@ -78,13 +128,18 @@ function iframeLoaded(){
             $(this).attr("src", "map/sat/egressy_map_click.png")
             $(this).addClass("selectedmap")
             $("#kivalasztott", parent.document).text($(this).attr("alt"))
+            $("#kivalasztott", parent.document).css("opacity", "1")
+            $("#info", parent.document).text(isktxt)
+            
         }
         else{
             $(this).removeClass("selectedmap")
             $(this).attr("src", "map/sat/egressy_map.png")
             $(this).css("left", "9px")
             $(this).css("top", "8px")
-            $("#kivalasztott", parent.document).text("")
+            $("#kivalasztott", parent.document).text("-")
+            $("#kivalasztott", parent.document).css("opacity", "0")
+            $("#info", parent.document).text("")
         }
     })
     $(".epu")
@@ -100,11 +155,30 @@ function iframeLoaded(){
             $(".selected").removeClass("selected")
             $(this).addClass("selected")
             $("#kivalasztott", parent.document).text($(this).attr("alt"))
+            $("#kivalasztott", parent.document).css("opacity", "1")
+            $("#info", parent.document).text("")
+            for(var i = 0; i < epuletekJSON.length; i++){
+                if(epuletekJSON[i].epulet == $(this).attr("id")){
+                    var para = $("<p>")
+                    para.text(`${epuletekJSON[i]["text"]}`)
+                    $("#info", parent.document).append(para)
+                    if(!epuletekJSON[i]["termek"].includes(null)){
+                        $("#info", parent.document).append(`<div class='row'>Termek:`)
+                        for(var j in epuletekJSON[i]["termek"]){
+                            $("#info", parent.document).append(`<div class='col'>${epuletekJSON[i]["termek"][j]}</div>`)
+                        }
+                        $("#info", parent.document).append(`</div>`)
+                    }
+                    break
+                }
+            }
         }
         else{
             galChange($("#map2"))
             $(".selected").removeClass("selected")
-            $("#kivalasztott", parent.document).text("")
+            $("#kivalasztott", parent.document).text("-")
+            $("#kivalasztott", parent.document).css("opacity", "0")
+            $("#info", parent.document).text("")
         }
     })
     $("#map1")
@@ -112,7 +186,9 @@ function iframeLoaded(){
         galChange($(this))
         $(".selected").removeClass("selected")
         $(".selectedmap").removeClass("selectedmap")
-        $("#kivalasztott", parent.document).text("")
+        $("#kivalasztott", parent.document).text("-")
+        $("#kivalasztott").css("opacity", "0")
+        $("#info", parent.document).text("")
     })
 
 }
@@ -120,7 +196,10 @@ function iframeLoaded(){
 
 function pageLoaded(){
 
+    /*JSON-ban adatkereső függvény*/
 
+
+    
 
     var pos = $(".kep").eq(0).position()["left"] - 210
     $("#galjobb").bind("click", function(){
